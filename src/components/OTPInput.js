c0218c24000a2c27e9a60921fbc16e08a02f8b9b
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-05 22:14:03 +0530

    feat: otp input component

--- src/components/OTPInput.js
+++ src/components/OTPInput.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const OTPInput = ({ length, handleOTPSubmit }) => {
-  return <div>OTPInput</div>;
+  const [OTP, setOTP] = useState(new Array(length).fill(""));
+
+  const inputRefs = useRef([]);
+
+  console.log(inputRefs);
+
+  useEffect(() => {
+    if (inputRefs.current[0]) {
+      inputRefs.current[0].focus();
+    }
+  }, []);
+
+  const handleChange = (index, event) => {
+    const { value } = event.target;
+
+    if (isNaN(value)) return;
+
+    const newOTP = [...OTP];
+
+    //allow only one input
+    newOTP[index] = value.substring(value.length - 1);
+    setOTP(newOTP);
+
+    //submit trigger
+
+    const combinedOTP = newOTP.join("");
+
+    if (combinedOTP.length === length) handleOTPSubmit(combinedOTP, newOTP);
+  };
+
+  const handleClick = (index) => {};
+
+  const handlekeyDown = () => {};
+
+  return (
+    <div>
+      {OTP.map((value, index) => {
+        return (
+          <input
+            key={index}
+            ref={(input) => {
+              inputRefs.current[index] = input;
+            }}
+            type="text"
+            value={value}
+            onChange={(event) => handleChange(index, event)}
+            onClick={() => handleClick(index)}
+            onKeyDown={(event) => handlekeyDown(index, event)}
+            className="otp-input"
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default OTPInput;
